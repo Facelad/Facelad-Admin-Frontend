7031e4108f6b7d4dd772e28b3d40a385030b4a6e
owen672 <tecnowebsolutions59@gmail.com>
2026-03-27 10:17:43 -0300

    Cambios en login y registro

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// Configuración de Vite
 export default defineConfig({
-  plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
-    },
+  plugins: [react()],
+  server: {
+    port: 3000, // puedes cambiar el puerto si lo necesitas
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-  assetsInclude: ['**/*.svg', '**/*.csv'],
-})
+});
